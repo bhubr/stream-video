8106933cef10e12511f968eb98efb9429c6b1d5c
Benoît Hubert <benoithubert@gmail.com>
2020-08-14 21:20:25 +0200

    Log using debug, make it the Express way

--- server/handlers/videos.js
+++ server/handlers/videos.js
@@ -1,18 +1,22 @@
+// https://stackoverflow.com/q/36715403/
 import mimeTypes from 'mime-types'
 import fs, { promises as fsp } from 'fs'
 import path from 'path'
+import debug from 'debug'
 import notFound from './notFound'
 import { privateDir } from '../config'
 
+const log = debug('videos')
+
 const fullVideosDir = path.resolve(__dirname, '../', privateDir)
-console.log(fullVideosDir)
+
 export default async (req, res) => {
   const range = req.headers.range
   const filename = req.url.substr(1)
   const contentType = mimeTypes.lookup(filename)
 
   const fullFilePath = path.join(fullVideosDir, filename)
-  console.log(fullFilePath)
+  log(fullFilePath)
 
   let stats
   try {
@@ -24,7 +28,6 @@ export default async (req, res) => {
   const { range: rangeHeader } = req.headers
   if (!rangeHeader) {
     const fileContent = await fsp.readFile(fullFilePath)
-    console.log('hei')
     res.status(200)
     res.set('Connection', 'keep-alive')
     res.set('Content-Type', contentType)
@@ -37,7 +40,7 @@ export default async (req, res) => {
   const ini = +split[1]
   const end = split[2] ? +split[2] : total - 1
   const chunkSize = end - ini + 1
-  console.log(ini, end, chunkSize)
+  log(`ini: ${ini}, end: ${end}, chunkSize: ${chunkSize}`)
   if (parseInt(ini) >= total || parseInt(end) >= total) {
     // Indicate the acceptable range.
     res.status(416)
@@ -45,12 +48,11 @@ export default async (req, res) => {
     // Return the 416 'Requested Range Not Satisfiable'.
     res.end()
   }
-  res.writeHead(206, {
-    Connection: 'keep-alive',
-    'Content-Range': 'bytes ' + ini + '-' + end + '/' + total,
-    'Accept-Ranges': 'bytes',
-    'Content-Length': chunkSize,
-    'Content-Type': contentType
-  })
+  res.status(206)
+  res.set('Connection', 'keep-alive')
+  res.set('Content-Range', 'bytes ' + ini + '-' + end + '/' + total)
+  res.set('Accept-Ranges', 'bytes')
+  res.set('Content-Length', chunkSize)
+  res.set('Content-Type', contentType)
   fs.createReadStream(fullFilePath, { start: ini, end: end }).pipe(res)
 }
