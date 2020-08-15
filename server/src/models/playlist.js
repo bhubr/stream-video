1a4196eda633310071f445da8ff0431ab15dd36f
Benoît Hubert <benoithubert@gmail.com>
2020-08-15 12:29:29 +0200

    Get playlist's folder for each video

--- server/src/models/playlist.js
+++ server/src/models/playlist.js
@@ -2,7 +2,11 @@ import dbQuery from '../dbQuery'
 
 export default {
   async findVideos(playlistId) {
-    const sql = 'SELECT * FROM video WHERE playlist_id = ?'
+    const sql = `
+    SELECT v.*, p.folder
+    FROM video v
+    JOIN playlist p ON v.playlist_id = p.id
+    WHERE playlist_id = ?`
     console.log(sql, playlistId)
     return dbQuery(sql, [playlistId])
   }
